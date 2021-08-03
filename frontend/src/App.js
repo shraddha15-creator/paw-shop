735641a552ecd3df2e39682f3c0914abd3a8e8aa
Shraddha Muley <66071175+shraddha15-creator@users.noreply.github.com>
2021-08-03 15:42:11 +0530

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+const App = () => {
+  return (
+    <>
+    <Header />
+      <h1>Welcome to PawShop!🐾</h1>
+    <Footer />
+    </>
+  );
+}
+
+export default App;
